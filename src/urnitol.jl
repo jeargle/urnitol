0e2e96373c84864142f8dc4c2c9427d3b9ba062b
John Eargle <jeargle@gmail.com>
2017-05-27 14:15:06 -0500

    added stochastic ball selection

--- src/urnitol.jl
+++ src/urnitol.jl
@@ -1,28 +1,40 @@
 # John Eargle (mailto: jeargle at gmail.com)
-# 2016
+# 2016-2017
 # Urnitol
 
 module Urnitol
 
-export EventBin, ProbArray, Ball, Urn, choose_event
+export EventBin, ProbArray, Urn, pull_ball, choose_event
 
-
-type Ball
-    color::AbstractString
-    count::Int
-end
 
 type Urn
     name::AbstractString
-    balls::Array{Ball, 1}
-    Urn(name::AbstractString) = new(name, Ball[])
+    balls::Dict{AbstractString, Int64}
+    Urn(name::AbstractString) = new(name, Dict())
     Urn(name::AbstractString, balls) = new(name, balls)
 end
 
 type EventBin
     name::AbstractString
-    balls::Array{Tuple{Ball, Urn}, 1}
-    EventBin(name::AbstractString) = new(name, Tuple{Ball, Urn}[])
+    balls::Dict{AbstractString, Int64}
+    EventBin(name::AbstractString, balls) = new(name, balls)
+end
+
+function pull_ball(urn)
+    total_balls = sum(values(urn.balls))
+    ball_idx = rand(1:total_balls)
+    ball_count = 0
+    chosen_balls = Dict()
+    for i in keys(urn.balls)
+        ball_count += urn.balls[i]
+        if ball_count >= ball_idx
+            urn.balls[i] -= 1
+            chosen_balls[i] = 1
+            break
+        end
+    end
+    
+    return chosen_balls
 end
 
 type ProbArray
--- test/test.jl
+++ test/test.jl
@@ -1,30 +1,37 @@
 # John Eargle (mailto: jeargle at gmail.com)
-# 2016
+# 2016-2017
 # test
 
 using Urnitol
 
-ebin1 = EventBin("bin1")
-ebin2 = EventBin("bin2")
+ebin1 = EventBin("bin1", Dict("black" => 0))
+ebin2 = EventBin("bin2", Dict("white" => 0))
 
 prob1 = ProbArray([ebin1, ebin2])
 prob2 = ProbArray([ebin1, ebin2, ebin2, ebin2])
 
-ball1 = Ball("black", 2)
-ball2 = Ball("white", 3)
-
 urn1 = Urn("urnie")
-urn2 = Urn("bert", [ball1, ball2])
+urn2 = Urn("bert", Dict("black" => 2, "white" => 3))
 
 println(urn1)
 println(urn2)
 
-println("prob1")
+println("*** prob1")
 for x = 1:10
     println(choose_event(prob1))
 end
 
-println("prob2")
+println("*** prob2")
 for x = 1:10
     println(choose_event(prob2))
 end
+
+println("*** choose 3 balls")
+println(urn2)
+ball = pull_ball(urn2)
+println(ball)
+ball = pull_ball(urn2)
+println(ball)
+ball = pull_ball(urn2)
+println(ball)
+println(urn2)
